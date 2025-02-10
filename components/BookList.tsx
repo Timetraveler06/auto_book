ab60d3982de5371d1282985d731ffde98c9b46fd
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-11 00:02:57 +0300

    Update BookList.tsx

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
-const BookList = () => {
+interface Props {
+  title: string;
+  books: Book[];
+  containerClassName?: string
+
+}
+
+const BookList = ({title, books, containerClassName}: Props) => {
   return (
     <section>
       <h2 className='text-light-100 text-4xl font-bebas-neue'>
