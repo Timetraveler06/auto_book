58338318ed451606109651872fcbe9b7099479a1
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-11 00:06:00 +0300

    Update BookList.tsx

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import BookCard from './BookCard';
 
 interface Props {
   title: string;
@@ -14,7 +15,11 @@ const BookList = ({title, books, containerClassName}: Props) => {
         {title}
       </h2>
       <ul className='book-list'>
-        
+        {books.map((book)=> (
+          <BookCard key={book.title} {...book} />
+          
+        ))}
+
       </ul>
     </section>
   )
