a3802983b3f057ce7cda45c36f1f8a4fb0f88c2e
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-11 11:55:09 +0300

    Layout of Auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,15 @@
 import React, { ReactNode } from 'react'
-
+import Image from "next/image";
 const layout = ({children}:{children: ReactNode}) => {
   return (
-    <div>layout</div>
+    <main className='auth-container'>
+        <section className='auth-form'>
+            <div className='auth-box'>
+                <Image src='/icons/logo.svg' alt='logo' width={37} height={37} />
+                <h2>BookWise</h2>
+            </div>
+        </section>
+    </main>
   )
 }
 
