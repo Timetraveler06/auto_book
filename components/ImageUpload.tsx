b08b178a2da9da9b53de1f21d43bb8bc2a4aa9aa
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-12 01:25:24 +0300

    Image Upload on work

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -46,10 +46,30 @@ const ImageUpload = () => {
 
         <IKUpload 
             className="hidden" ref={ikUploadRef} onError={onError} onSuccess={onSuccess} fileName="test-upload.png" />
-            <button className="upload-btn flex items-center justify-center p-2">
+            <button className="upload-btn " onClick={(e)=>{
+                e.preventDefault();
+
+
+                if(ikUploadRef.current) {
+                    // @ts-ignore
+                    ikUploadRef.current?.click();
+                }
+            }}>
                 <Image src="/icons/upload.svg" alt="upload-icon" width={20} height={20}
-                 className="object-contain  "/>
+                 className="object-contain "/>
+                 <p className="text-base text-light-100">Upload a File</p>
+
+                 {file && <p className="upload-filename">{file.filePath}</p>}
             </button>
+
+            { file && (
+                <IKImage
+                    alt={file.filePath}
+                    path ={file.filePath}
+                    width={500}
+                    height={500}
+                    />
+            )}
     </ImageKitProvider>
   )
 
