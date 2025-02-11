9d3c6dd630c9c2a3a2e693f62070135157f0c892
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-11 11:56:32 +0300

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,8 +5,11 @@ const layout = ({children}:{children: ReactNode}) => {
     <main className='auth-container'>
         <section className='auth-form'>
             <div className='auth-box'>
-                <Image src='/icons/logo.svg' alt='logo' width={37} height={37} />
-                <h2>BookWise</h2>
+                <div className='flex flex-row gap-2'>
+                    <Image src='/icons/logo.svg' alt='logo' width={37} height={37} />
+                    <h2 className='text-2xl font-semibold text-white'>BookWise</h2>
+                </div>
+                
             </div>
         </section>
     </main>
