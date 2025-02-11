8a5d319902366494645b828044761354ef3de699
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-11 21:59:46 +0300

    Update ImageUpload.tsx

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,5 +1,18 @@
-import React from 'react'
+"use client"
 import { IKImage, ImageKitProvider, IKUpload } from "imagekitio-next";
+
+
+
+const authenticator = async()=>{
+    try {
+        const response = await fetch();
+        
+    } catch (error: any) {
+        throw new Error(`Authentication request failed: ${error.message}`);
+        
+    }
+
+}
 const ImageUpload = () => {
   return (
     <div>ImageUpload</div>
