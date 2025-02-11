0460417e30cc9085904ee60bb9c2e14f275b7ca7
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-11 22:41:29 +0300

    Update route.ts

--- app/api/auth/imagekit/route.ts
+++ app/api/auth/imagekit/route.ts
@@ -1,9 +1,15 @@
 import ImageKit from "imagekit";
+import { NextResponse } from "next/server";
 
-const {env: { imagekit: { publicKey,privateKey,urlEndpoint}}} = config;
+const { env: { imagekit: { publicKey , privateKey, urlEndpoint },},} = config;
 
 const imagekit = new ImageKit({
-    publicKey: publicKey,
-    privateKey: privateKey,
-    urlEndpoint: urlEndpoint,
-});
+    publicKey,
+    privateKey,
+    urlEndpoint,
+}); 
+
+
+export async function GET(){
+    return NextResponse.json(imagekit.getAuthenticationParameters());
+}
