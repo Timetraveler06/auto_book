8b0aa13b48fe124c52c0586fe71386c058c0f998
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-11 22:09:10 +0300

    Doing Image Upload

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -5,7 +5,17 @@ import { IKImage, ImageKitProvider, IKUpload } from "imagekitio-next";
 
 const authenticator = async()=>{
     try {
-        const response = await fetch();
+        const response = await fetch(`${config.env.apiEndpoint}/api/auth.imagekit`);
+
+        if(!response.ok){
+            const errorText = await response.text();
+            throw new Error(`Request failed with status:  ${response.status}: ${errorText}`);
+        }
+
+        const data = await response.json();
+        const {signature, expire, token} = data;
+
+        return { token , expire , signature };
         
     } catch (error: any) {
         throw new Error(`Authentication request failed: ${error.message}`);
