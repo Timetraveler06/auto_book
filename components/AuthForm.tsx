0fed02cc70417eb448d73091bb0282ad2e931bca
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-11 12:04:07 +0300

    Create AuthForm.tsx

---
+++ components/AuthForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+//Create a Generic Interfce 
+interface Props
+const AuthForm = ({ type, schema, defaultValues, onSubmit}: Props) => {
+  return (
+    <div>AuthForm</div>
+  )
+}
+
+export default AuthForm
