d690155f9203c98d0eb702794b1ab4e1b253523b
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-11 22:47:07 +0300

    Update ImageUpload.tsx

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,6 +1,14 @@
 "use client"
+import ImageKit from "imagekit";
 import { IKImage, ImageKitProvider, IKUpload } from "imagekitio-next";
 
+const { env: { imagekit: { publicKey , privateKey, urlEndpoint },},} = config;
+
+const imagekit = new ImageKit({
+    publicKey,
+    privateKey,
+    urlEndpoint,
+}); 
 
 
 const authenticator = async()=>{
@@ -15,7 +23,7 @@ const authenticator = async()=>{
         const data = await response.json();
         const {signature, expire, token} = data;
 
-        return { token , expire , signature };
+        return { token , expire , signature};
         
     } catch (error: any) {
         throw new Error(`Authentication request failed: ${error.message}`);
@@ -24,9 +32,16 @@ const authenticator = async()=>{
 
 }
 const ImageUpload = () => {
-  return (
-    <div>ImageUpload</div>
-  )
+
+    const ikUploadRef = useRef(null)
+
+  return 
+  <ImageKitProvider publicKey={publicKey} urlEndpoint={urlEndpoint} authenticator={authenticator} >
+
+    <IKUpload />
+
+</ImageKitProvider>
+
 }
 
 export default ImageUpload
