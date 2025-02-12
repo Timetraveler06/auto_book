1efb02ebb38f621ac0ac96a76930955f05fedfab
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-12 12:14:49 +0300

    DB Config

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,27 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import { sampleBooks } from "@/constants";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 
 
 
-const Home=()=> (
+const Home= async ()=> {
+
+
+   const result = await db.select().from(users);
  
-   <>
-      <BookOverview {...sampleBooks[0]}
-      
-      />
-      <BookList 
-         title="Latest Books"
-         books={sampleBooks}
-         containerClassName='mt-28' />
-   </>
+   return (  
+      <>
+         <BookOverview {...sampleBooks[0]} />
+         <BookList 
+            title="Latest Books"
+            books={sampleBooks}
+            containerClassName='mt-28' 
+         />
+      </>
+   )
   
-);
+};
 
 export default Home;
