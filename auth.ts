58afc60581cb15db9a793218769318fda674af37
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-13 00:15:38 +0300

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,5 +1,23 @@
 import NextAuth from "next-auth"
- 
+ import CredentialProvider from 'next-auth/providers/credentials';
+import { db } from "./database/drizzle";
+import { eq } from "drizzle-orm";
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+    session:{
+        strategy:"jwt",
+    }, 
+  providers: [
+    CredentialProvider({
+        async authorize(credientials){
+            if(!credientials?.email ||!credientials?.password ){
+                return null;
+            }
+
+            const user = await db
+                .select()
+                .from(users)
+                .where(eq("email", credientials.email.toString()))
+        }
+    })
+  ],
 })
