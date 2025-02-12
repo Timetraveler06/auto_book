a286263c721356b93f4b1c41d2b63a48002972d5
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-13 00:18:36 +0300

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
  import CredentialProvider from 'next-auth/providers/credentials';
+ import {compare} from "bcryptjs"
 import { db } from "./database/drizzle";
 import { eq } from "drizzle-orm";
 import { users } from "./database/schema";
@@ -18,7 +19,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                 .select()
                 .from(users)
                 .where(eq(users.email, credientials.email.toString()))
-                .limit(1)
+                .limit(1);
+
+
+            if(user.length == 0) return null;
+
+            const isPasswordValid = await compare(credientials.password.toString(),user[0].password)
         }
     })
   ],
