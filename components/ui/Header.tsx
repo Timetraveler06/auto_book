d03984e83383acf7c51379a90456ec7bb98a87cd
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-13 15:30:02 +0300

    updating

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,31 +1,49 @@
-"use client";  // Client Rendering COmponent
-import { cn } from '@/lib/utils';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import React from 'react';
-import Image  from "next/image";
-const Header = () => {
+"use client";
+import { cn, getInitials } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import Image from "next/image";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Session } from "next-auth";
 
-    const pathName = usePathname()
+const Header = ({ session }: { session: Session }) => {
+  const pathName = usePathname();
+
   return (
-    <header className='my-10 flex justify-between gap-5'>
-        <Link href='/'>
-            <Image src="/icons/logo.svg" alt="logo" width={40} height={40} />
-         </Link>
+    <header className="my-10 flex justify-between items-center">
+      {/* Logo */}
+      <Link href="/">
+        <Image src="/icons/logo.svg" alt="logo" width={40} height={40} />
+      </Link>
 
-        <ul className='flex flex-row itemns-center gap-8'>
-            <li>
-                <Link href="/library" className={cn( 'text-base cursor-pointer capitalize' ,
-                    pathName=='/library' ? 'text-light-200' : 'text-light-100' )} >
-                    Library
-                </Link>
-            </li>
-            <li>
-                <Link href="/my-profile" > </Link>
-            </li>
-        </ul>
+      {/* Navbar */}
+      <ul className="flex items-center gap-8">
+        <li>
+          <Link
+            href="/library"
+            className={cn(
+              "text-base cursor-pointer capitalize",
+              pathName === "/library" ? "text-light-200" : "text-light-100"
+            )}
+          >
+            Library
+          </Link>
+        </li>
+
+        {/* Profile Avatar */}
+        <li>
+          <Link href="/my-profile">
+            <Avatar className="bg-amber-100 w-10 h-10 flex items-center justify-center">
+              <AvatarFallback className="bg-amber-100 font-bold">
+                {getInitials(session?.user?.name || "IN")}
+              </AvatarFallback>
+            </Avatar>
+          </Link>
+        </li>
+      </ul>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
