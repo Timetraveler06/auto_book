dd3f4617e8469aa205dbc0d31c0090a5cff6c192
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-14 00:43:35 +0300

    Started with Upstash
    For DB optimization and rate limit

---
+++ app/(root)/my-profile/page.tsx
@@ -0,0 +1,21 @@
+import { signOut } from '@/auth';
+import BookList from '@/components/BookList';
+import { Button } from '@/components/ui/button'
+import { sampleBooks } from '@/constants';
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <form  action={async () =>{
+            'use server';
+            await signOut();
+        }} className='mb-10' >
+            <Button className='px-10'>Log out</Button>
+        </form>
+        <BookList title='Borrowed Books' books={sampleBooks}/>
+    </div>
+  )
+}
+
+export default page
