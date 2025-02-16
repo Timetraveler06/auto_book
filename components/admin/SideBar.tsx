bbf118c9a67ff868c7552655cb3a222ddee42e50
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-16 22:59:39 +0300

    Update SideBar.tsx

--- components/admin/SideBar.tsx
+++ components/admin/SideBar.tsx
@@ -1,8 +1,36 @@
+import { adminSideBarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <div className='admin-sidebar '>
+        <div>
+            <div className='logo'>
+                <Image src="/icons/admin/logo.svg" alt='logo' height={37} width={37} />
+                <h1>Auto Book</h1>
+            </div>
+
+            <div className='mt-10 flex flex-row gap-5'>
+                { adminSideBarLinks.map((link)=>{
+                    const isSelected = false;
+                    return (
+                        <Link href={link.route} key={link.route}>
+                           <div className={cn('link',isSelected && 'bg-primary-admin shadow-sm')} >
+                                <div className='relative size-5 '>
+                                    <Image src={link.img} alt='icon' fill className={`${isSelected} ? 'brightness-0 invert ' : '' object-contain` } />
+                                </div>
+                            </div> 
+                        </Link>
+                    )
+                })
+
+                }
+            </div>
+        </div>
+    </div>
   )
 }
 
