a8b27bf6544f551922c2e4ab8c733ec0daef1538
Josi <101510934+Timetraveler06@users.noreply.github.com>
2025-02-16 21:27:45 +0300

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 
-const layout = () => {
+const layout = ({children}:{children:ReactNode}) => {
   return (
-    <div>layout</div>
+   <main className='flex min-h-screen w-full flex-row'>
+        <p>Sidebar</p>
+        <div className='admin-container'>
+            <p>Header</p>
+            {children}
+        </div>
+   </main>
   )
 }
 
